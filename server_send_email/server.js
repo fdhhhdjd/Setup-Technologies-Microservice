305b6cf132ed0dc45c5be026f64aa5d5fb2c5d32
tai <nguyentientai10@gmail.com>
2022-09-29 17:57:28 +0700

    redis-pub-sub complete

---
+++ server_send_email/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const REDIS = require("./db/redis");
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+// REDIS.subscribe("ordersystem");
+// REDIS.on("message", (channel, message) => {
+//   console.log("Channel Send Email", channel);
+//   console.log("Message Send Email", JSON.parse(message));
+// });
+REDIS.psubscribe("o*");
+REDIS.on("pmessage", (pattern, channel, message) => {
+  console.log("Pattern Send Email", pattern);
+  console.log("Channel Send Email", channel);
+  console.log("Message Send Email", JSON.parse(message));
+});
+app.get("/", async (req, res) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "Tài Đẹp trai ",
+    timestamp: Date.now(),
+  };
+  return res.send(healthcheck);
+});
+
+//!Link router Main
+
+const PORT = process.env.PORT || 5003;
+app.listen(PORT);
+console.log(`Running on http://localhost:${PORT}`);
--- server_transaction/server.js
+++ server_transaction/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 var amqp = require("amqplib");
 dotenv.config();
 const connectDB = require("./db/mongo");
+const REDIS = require("./db/redis");
 connectDB();
 var connection, channel;
 async function connect_amqp() {
@@ -14,6 +15,7 @@ async function connect_amqp() {
 connect_amqp().then(() => {
   channel.consume("ORDER_PRODUCT", (data) => {
     console.log("Order Product Success:::", JSON.parse(data.content));
+    REDIS.publish("ordersystem", JSON.stringify(JSON.parse(data.content)));
   });
 });
 const app = express();
